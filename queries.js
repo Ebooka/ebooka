5b0b7300ad45636e8f13e02ab5a14beec6ba11f1
iuke <edamm@itba.edu.ar>
2023-10-22 15:18:02 -0300

    to deploy

--- queries.js
+++ queries.js
@@ -2,9 +2,25 @@ const { Pool } = require('pg');
 const config = require('config');
 const connectionString = config.get('postgresURI');
 
+console.log('process env', process.env.DB_URL);
+
 const pool = new Pool({
-    connectionString: connectionString,
-    ssl: true,
+    connectionString: process.env.DB_URL,
+    ssl: process.env.NODE_ENV === 'production',
 });
+
+test();
+
+async function test() {
+    const client = await pool.connect();
+    try {
+        const res = await client.query('SELECT * FROM users');
+        console.log(res.rows);
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        client.release();
+    }
+}
 
 module.exports = { pool }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const config = require('config');
@@ -20,7 +21,7 @@ app.use('/api/favourites', require('./endpoints/api/favourites'));
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
+    app.get('*', (_, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
